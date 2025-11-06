74fefe56a28155433e94bb48dc8d9c8123ddd4be
Tania Kwok <tania@pinkmojito.net>
2025-11-06 23:11:42 +0400

    Show vehicle page done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
   # VALIDATIONS
   validates :rating, numericality: { only_integer: true, in: 0..5 }
+  validates :comment, presence: true
 
   # ASSOCIATIONS
   belongs_to :user
